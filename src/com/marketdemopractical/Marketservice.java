682515bdd2d7295a723ee775bed32f9761856510
man78 <manish.peddurwar@gmail.com>
2022-12-14 15:54:23 +0530

    changes in src market service by mani

--- src/com/marketdemopractical/Marketservice.java
+++ src/com/marketdemopractical/Marketservice.java
@@ -2,7 +2,7 @@ package com.marketdemopractical;
 
 public class Marketservice {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello what the fuck");
 	}
 
 }
